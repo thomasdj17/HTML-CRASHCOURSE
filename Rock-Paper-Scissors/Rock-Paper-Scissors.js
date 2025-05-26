dfedbb1497f4107c3670966fecce41be87d66361
thomasdj17 <thomasdj17@yahoo.com>
2025-05-25 20:37:43 -0500

    rock-paper-scissors 	modified:   Rock-Paper-Scissors/Rock-Paper-Scissors.css 	modified:   Rock-Paper-Scissors/Rock-Paper-Scissors.js

--- Rock-Paper-Scissors/Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors/Rock-Paper-Scissors.js
@@ -4,19 +4,29 @@ const scissors = document.getElementById("Scissors");
 
 function push() {
     if (rock && scissors) {
-        const result = ("Rock is the winner")
+        var result = "Rock is the winner!!!";
     }   
     if (rock && paper) {
-        const result = ("Paper is the winner")
+        var result = "Paper is the winner!!!";
     }   
     if (paper && scissors) {
-        const result = ("Scissors is the winner")
+        var result = "Scissors is the winner!!!";
     }   
     if (scissors && scissors || rock && rock || paper && paper) {
-        const result = ("It's a tie")
+        var result = "It's a tie!!!";
     }
-    const text = result;
-    const  = document.getElementById("winner").innerHTML;
+    
+    document.getElementById("winner").innerHTML = result;
+}
 
-    text.appendChild("result")
+const user = 1;
+const cpu = 2;
+const winner = choices;
+
+function score() {
+    if (winner === user) {
+        let (document.getElementById("Wins").value = i++ )
+    } else {
+        let (document.getElementById("Loses").value = i++)
+    }
 }
