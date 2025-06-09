5a19349f17251a49e87c9f2b626eac4279c8ffab
thomasdj17 <thomasdj17@yahoo.com>
2025-06-09 18:37:57 -0500

    memoryGame

--- Memory Game/Memory Game.js
+++ Memory Game/Memory Game.js
@@ -3,13 +3,14 @@ function getRandQuestions() {
     return questions [Math.floor(Math.random() * questions.length)];
 }
 
-function quest() {
+function startButton() {
     document.getElementById("questions").innerHTML = getRandQuestions();
     document.getElementById("tasks").innerHTML = "";
 }
 
 let timeLeft = 5;
 const timerDisplay = document.getElementById("timer");
+let timerInterval;
 
 function countdown() {
     if (timeLeft >= 0) {
@@ -20,4 +21,12 @@ function countdown() {
     }
 }
 
-const timerInterval = setInterval(countdown, 1000);
+document.getElementById("startButton").addEventListener("click", function() {
+    clearInterval(timerInterval);
+    timeLeft = 5;
+    timerInterval = setInterval(countdown, 1000);
+    if(timerInterval === 0) {
+        document.getElementById("questions").innerHTML = "";
+        
+    }
+});
